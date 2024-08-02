6d2ac78f0abc80b770f682bdc672a4f79152c7de
zzwqrd <zeinelabdeen21@gmail.com>
2024-08-02 21:55:50 +0300

    ui screen WelcomeView

---
+++ lib/generated/locale_keys.g.dart
@@ -0,0 +1,16 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+abstract class  LocaleKeys {
+  static const change_language = 'change_language';
+  static const welcome = 'welcome';
+  static const sub_welcome = 'sub_welcome';
+  static const login = 'login';
+  static const register = 'register';
+  static const guest = 'guest';
+  static const language_changed = 'language_changed';
+  static const confirm = 'confirm';
+  static const confirm_language_change = 'confirm_language_change';
+  static const cancel = 'cancel';
+  static const ok = 'ok';
+
+}
